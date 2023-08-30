c508ff131f49dc98d0259e9725a334705d291f78
Aviv Lugasi <alugasi@redhat.com>
2023-08-30 16:15:57 +0300

    added nodes param in boot.py, change defualt iso_url

--- boot.py
+++ boot.py
@@ -6,6 +6,7 @@ import yaml
 import socket
 
 inventory = sys.argv[1] if len(sys.argv) > 1 else 'inventory'
+nodes = int(sys.argv[2]) if len(sys.argv) > 2 else 3
 with open(inventory) as f:
     data = yaml.safe_load(f)['all']['vars']
 
@@ -17,11 +18,14 @@ default_bmc_model = data.get('bmc_model', 'dell')
 iso_url = data.get('iso_url')
 if not iso_url:
     ipaddr = socket.gethostbyname(socket.gethostname())
-    iso_url = f"http://{ipaddr}/helix50.iso"
+    cluster = data.get('cluster')
+    iso_url = f"http://{ipaddr}/{cluster}.iso"
 print ("****************** DEBUG ************** isourl = ", iso_url)
 
 hosts = data['hosts']
-for host in hosts:
+for index, host in enumerate(hosts):
+    if index >= nodes:
+        break
     bmc_url = host.get('bmc_url')
     bmc_user = host.get('bmc_user', default_bmc_user)
     bmc_password = host.get('bmc_password', default_bmc_password)
